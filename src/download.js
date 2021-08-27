48ffcf299bc76b68c459b136901cdbccb78206f3
Marilyne <marilyne.summo@cirad.fr>
2021-08-27 11:52:25 +0200

    add svg format to download

--- src/download.js
+++ src/download.js
@@ -7,19 +7,35 @@ export function downloadArchive(){
     let chart = $('#_ideogram')
         .attr('xmlns', 'http://www.w3.org/2000/svg');
     //dimensions
-    var bBox = chart.get(0).getBBox();
-    
+    //var bBox = chart.get(0).getBBox();
+    //console.log("bbox heigth "+bBox.height+ "bbox width "+bBox.width);
+    let width = parseInt(chart.css("width").replace("px", ""))+100;
+    let height = parseInt(chart.css("height").replace("px", ""))+100;
+    console.log(" height "+height+ " width "+width);
+
     var data = new XMLSerializer().serializeToString(chart.get(0));
     var svg = new Blob([data], { type: "image/svg+xml;charset=utf-8" });
     var url = URL.createObjectURL(svg);
 
+
+//////////SVG
+    var svg_data = document.getElementById("_ideogram").innerHTML //put id of your svg element here
+    var head = '<svg title="graph" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+
+    //if you have some additional styling like graph edges put them inside <style> tag
+    var style = '<style>circle {cursor: pointer;stroke-width: 1.5px;}text {font: 10px arial;}path {stroke: DimGrey;stroke-width: 1.5px;}.bands{fill: lightgrey}</style>'
+    var full_svg = head +  style + svg_data + "</svg>"
+    var blob_svg = new Blob([full_svg], {type: "image/svg+xml"});  
+
+///////////
+
     var img = $('<img />')
-        .width(bBox.width+100)
-        .height(bBox.height+100)
+        .width(width)
+        .height(height)
         .on('load', function() {
             var canvas = document.createElement('canvas');
-            canvas.width = bBox.width+100;
-            canvas.height = bBox.height+100;
+            canvas.width = width;
+            canvas.height = height;
             var ctx = canvas.getContext('2d');
             //background
             ctx.fillStyle = "white";
@@ -30,11 +46,12 @@ export function downloadArchive(){
             canvas.toBlob(function(blob) {
                 var zip = new JSZip();
                 zip.file("gemo.png", blob); // <-- JSZip v3 accepts blob
+                zip.file("gemo.svg", blob_svg); // <-- JSZip v3 accepts blob
                 //retreive other data from form
                 zip.file("input.txt", $("#editorAnnot").val());
                 zip.file("chromosomes.txt", $("#editorChr").val());
                 zip.file("color.txt", $("#editorColor").val());
-
+                
                 //generate zip
                 let content = zip.generateAsync({type:"blob"}).then(function (blob) {
                     saveAs(blob, "gemo.zip"); // <-- trigger the download
