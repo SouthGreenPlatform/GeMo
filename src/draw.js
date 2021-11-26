066b592523d97c1720734a597d6650af3513ec40
Marilyne <marilyne.summo@cirad.fr>
2021-11-26 16:39:30 +0100

    update shape of bed annotations

--- src/draw.js
+++ src/draw.js
@@ -20,7 +20,7 @@ export function drawBed(bedAnnot, maxLength){
 			if(width<minWidth){width = minWidth;}
 
 			//draw
-			console.log(obj.chr);
+/* 			console.log(obj.chr);
 			d3.selectAll(".chromosome-set").select("[id^='chr"+obj.chr+"-']")
 			.append("rect")       // attach a rectangle
 			.style("fill", "black")
@@ -29,7 +29,18 @@ export function drawBed(bedAnnot, maxLength){
 			.attr("x", parseInt(start))         // position the left of the rectangle
 			.attr("y", -10)          // position the top of the rectangle
 			.attr("height", 10)    // set the height
-			.attr("width", width); 
+			.attr("width", width);  */
+
+			d3.selectAll(".chromosome-set").select("[id^='chr"+obj.chr+"-']")
+			.append("svg:image")
+			.attr('title', "Name: "+obj.name+" \nStart: "+obj.start+" Stop: "+obj.stop)
+			.attr('class', 'bed-annot')
+			.attr('x', parseInt(start)-7)
+			.attr('y', -15)
+			.attr('height', 15)
+			.attr("xlink:href", "public/img/location-sign.png");
+
+
 
 		
 		});
