51a83ece8c2b3c5a7eadb6dccdd655d815dc34ad
gdroc <gaetan.droc@cirad.fr>
2021-12-09 11:40:36 +0100

    Fix warning for _static page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,8 +102,8 @@ html_favicon = '_images/favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
 
+html_static_path = []
 # If false, no index is generated.
 html_use_index = True
 
